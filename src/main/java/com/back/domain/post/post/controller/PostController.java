a00084a10ace41481a68500d553db29ab9b579b1
5s-mini <dnclsehd123@naver.com>
2026-03-10 11:18:10 +0900

    feat: a 엘리먼트를 통해서 @PostMapping/@DeleteMapping/@PutMapping 이 가능하도록 만들어주는 JS 적용

--- src/main/java/com/back/domain/post/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/post/controller/PostController.java
@@ -10,10 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -71,7 +68,7 @@ public class PostController {
         return "modify";
     }
 
-    @PostMapping("/posts/{id}/modify")
+    @PutMapping("/posts/{id}/modify")
     @Transactional
     public String modify(@PathVariable int id,
                          @Valid @ModelAttribute("form") ModifyRequestForm form,
@@ -83,6 +80,12 @@ public class PostController {
 
         Post post = postService.modify(id, form.title, form.content);
         return "redirect:/posts/%d".formatted(post.getId()); // GET요청
+    }
+
+    @DeleteMapping("/posts/{id}/delete")
+    public String delete(@PathVariable int id) {
+        postService.deleteById(id);
+        return "redirect:/posts";
     }
 
     @GetMapping("/posts")
